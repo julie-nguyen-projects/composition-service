4b3352f88e4686fb63950ca5453154014c9e528f
Julie Nguyen <julie_nguyen@live.fr>
2018-05-14 17:06:59 +0200

    Added API for get all compositions

--- src/main/java/com/epitech/screen/fleet/compositionservice/Controller/CompositionController.java
+++ src/main/java/com/epitech/screen/fleet/compositionservice/Controller/CompositionController.java
@@ -1,9 +1,22 @@
 package com.epitech.screen.fleet.compositionservice.Controller;
 
+import com.epitech.screen.fleet.compositionservice.Model.Composition;
+import com.epitech.screen.fleet.compositionservice.Repository.CompositionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CompositionController {
 
+    @Autowired
+    private CompositionRepository compositionRepository;
+
+    @GetMapping("/compositions")
+    public List<Composition> getAllCompositions() {
+        return compositionRepository.findAll();
+    }
 
 }
