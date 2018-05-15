7302701a6362863a810201f372499067737e8fdf
Julie Nguyen <julie_nguyen@live.fr>
2018-05-15 14:40:49 +0200

    edited sql insertion when the app is launched

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,19 @@ INSERT INTO split_view(id, type_split)
 VALUES (110001, 1);
 INSERT INTO split_view(id, type_split)
 VALUES (110002, 0);
+
+INSERT INTO slide_view(id, nb_slides)
+VALUES (120001, 1);
+INSERT INTO slide_view(id, nb_slides)
+VALUES (120002, 3);
+INSERT INTO slide_view(id, nb_slides)
+VALUES (120003, 15);
+INSERT INTO slide_view(id, nb_slides)
+VALUES (120005, 25);
+
+INSERT into base_module(id)
+VALUES (13001);
+INSERT into base_module(id)
+VALUES (13002);
+INSERT into base_module(id)
+VALUES (13003);
